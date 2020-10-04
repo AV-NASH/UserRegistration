f91d7a605631d21f396a426aba2947cfbc270ad6
AV-NASH <avinash21213434@gmail.com>
2020-10-04 18:46:55 +0530

    Adding method to check first name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,8 +1,31 @@
 package com.cg.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the user registration program");
+		System.out.println("Welcome to the user registration program\n");
+		userInterface();
+	}
+
+	private static void userInterface() {
+		String firstname = checkFirstName();
+	}
+
+	private static String checkFirstName() {
+		String firstname;
+		boolean check;
+		do {
+			System.out.println("enter first name");
+			firstname = scanner.nextLine();
+			check = Pattern.matches("(^[A-Z])([a-zA-Z]{3})", firstname);
+			if (!check)
+				System.out.println("Invalid name please enter valid name");
+		} while (!check);
+		return firstname;
+
 	}
 }
